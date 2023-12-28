446f37454138b0c783870e1012ca24bac929a5c9
Matthew Kehrli <mdkehrli@gmail.com>
2023-12-27 17:31:54 -0800

    Update Extrapolate_ADG_VIStoUV_Test.m

--- Extrapolate_ADG_VIStoUV_Test.m
+++ Extrapolate_ADG_VIStoUV_Test.m
@@ -1,8 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Test script for the ADG extrapolation model code called
 %"Extrapolate_ADG_VIStoUV”. The Extrapolate_ADG_VIStoUV code is run for one
-%specified input of ag and ad at specified light wavelengths
-%(lambda). The resulting output from the test script is saved to
+%specified input of ag and ad at specified light wavelengths (lambda). The
+%resulting output from the test script is saved to
 %Extrapolate_ADG_VIStoUV_Test_Run_yyyymmdd.xls for comparison with the
 %provided output file Extrapolate_ADG_VIStoUV_Test_Run.xls.
 %
